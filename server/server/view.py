10f646db612c26fd925cf42155f8fac51dc656b1
baidaogui <baidaogui@sogou-inc.com>
2018-04-12 20:59:12 +0800

    search rest home through rh_ylw_id and rh_name
    N/A
    
    .

--- server/server/view.py
+++ server/server/view.py
@@ -5,35 +5,59 @@ import sys
 from django.http import HttpResponse
 from django.shortcuts import render
 
-sys.path.append("../../")
-sys.path.append("./")
-sys.path.append("../")
-
-reload(sys)
-
-from sql import RhSql
 from RHModel.models import rh
  
 curr_index = 0
+
 def start(request):
     global curr_index
     context = {}
     if "pre_page" in request.POST:
-        context['message'] = 'pre_page'#get_pre_page()
-        curr_index = curr_index - 1
+        get_pre_page(context)
     if "next_page" in request.POST:
-        context['message'] = 'next_page'#get_next_page()
-        curr_index = curr_index + 1
-    if "q" in request.POST:
-        print('q in request.POST')
-        context['message'] = "you input: " + request.POST['q']
+        get_next_page(context)
+    if "q_id" in request.POST:
+        get_page_from_rh_ylw_id(context, request.POST['q_id'])
+    if "q_name" in request.POST:
+        get_page_from_rh_name(context, request.POST['q_name'])
 
+    return render(request, 'index.html', context)
+
+def get_pre_page(context):
+    global curr_index
+    context['message'] = 'pre_page'
+    curr_index = curr_index - 1
+    # FIXME, should not query all DB
     db = rh.objects.all()
-    print(db[0])
     if curr_index <= 0:
         curr_index = 0
     context['record'] = db[curr_index]
-    return render(request, 'index.html', context)
+
+def get_next_page(context):
+    global curr_index
+    context['message'] = 'next_page'
+    # FIXME, should not query all DB
+    db = rh.objects.all()
+    curr_index = curr_index + 1
+    context['record'] = db[curr_index]
+
+def get_page_from_rh_ylw_id(context, ylw_id):
+    context['message'] = 'you are querying ylw id: ' + ylw_id
+    db = rh.objects.filter(rh_ylw_id=ylw_id)
+    if len(db) > 0:
+        # FIXME, should not query only one
+        context['record'] = db[0]
+    else:
+        context['message'] = context['message'] + ', Not Existed'
+
+def get_page_from_rh_name(context, rh_name):
+    context['message'] = 'you are querying yly name: ' + rh_name
+    db = rh.objects.filter(rh_name=rh_name)
+    if len(db) > 0:
+        # FIXME, should not query only one
+        context['record'] = db[0]
+    else:
+        context['message'] = context['message'] + ', Not Existed'
 
 def search(request):
     return start(request)
