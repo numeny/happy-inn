58208b8a60167bec69598e5ab05ef86cbb5a719b
bdg <1926366608@qq.com>
2018-04-12 09:00:12 +0800

    add next and pre page for web page
    N/A
    
    .

--- server/server/view.py
+++ server/server/view.py
@@ -5,8 +5,14 @@ from django.shortcuts import render
  
 def start(request):
     context = {}
+    if "pre_page" in request.POST:
+        context['message'] = 'pre_page'#get_pre_page()
+    if "next_page" in request.POST:
+        context['message'] = 'next_page'#get_next_page()
     if "q" in request.POST:
+        print('q in request.POST')
         context['message'] = "you input: " + request.POST['q']
+
     return render(request, 'index.html', context)
 
 def search(request):
